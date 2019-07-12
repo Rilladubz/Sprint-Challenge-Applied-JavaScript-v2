a582750133ebda20e5e5fb15fa43a2ecca7c5c87
Samuel Torres <rillatube@gmail.com>
2019-07-12 14:51:07 -0400

    End Day Commit

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,22 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function Tab(topic) {
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = topic;
+    return tab;
+}
+
+const topics = document.querySelector('.topics');
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        response.data.topics.forEach(topic => {
+            topics.appendChild(Tab(topic));
+        });
+    })
+    .catch(err => console.error(err));
+
